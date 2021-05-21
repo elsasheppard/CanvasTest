5e2db22468d1f02e9db3f249d038c1ac79a08451
Elsa Sheppard <sheppard.elsa03@gmail.com>
2021-05-21 12:59:47 -0700

    Start adding toolbar

--- app/src/main/java/com/example/canvastest/MainActivity.kt
+++ app/src/main/java/com/example/canvastest/MainActivity.kt
@@ -2,18 +2,44 @@ package com.example.canvastest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
+    // toolbar vars
+    private lateinit var mtoolbarBottom: Toolbar
+    private lateinit var myCanvasView : MyCanvasView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val myCanvasView = MyCanvasView(this)
+
+        myCanvasView =  MyCanvasView(this)
 
         // this makes drawing view fulscreen. not sure why its crossed out tho
         myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
         myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
-        setContentView(myCanvasView)
+        setContentView(R.layout.activity_main)
 
+        // toolbar functions
+        mtoolbarBottom = findViewById(R.id.toolbar_bottom)
+        mtoolbarBottom.inflateMenu(R.menu.menu_drawing)
+        mtoolbarBottom.setOnMenuItemClickListener(Toolbar.OnMenuItemClickListener { item ->
+            handleDrawingIconTouched(item.itemId)
+            false
+        })
+    }
+
+    // toolbar function
+    private fun handleDrawingIconTouched(itemId: Int) {
+        when (itemId) {
+            R.id.action_brush -> {
+                myCanvasView?.setOnTouchListener { view, motionEvent ->
+                    myCanvasView?.performClick(motionEvent)!!
+
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/canvastest/MyCanvasView.kt
+++ app/src/main/java/com/example/canvastest/MyCanvasView.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
+import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
@@ -12,17 +13,19 @@ import androidx.core.content.res.ResourcesCompat
 
 private const val STROKE_WIDTH = 12f // has to be float
 
-class MyCanvasView(context: Context) : View(context) {
+class MyCanvasView(context: Context, attributeSet : AttributeSet) : View(context) {
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
-    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
-    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+    private val backgroundColor = ResourcesCompat.getColor(context.resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(context.resources, R.color.colorPaint, null)
     private var path = Path()
     private var motionTouchEventX = 0f
     private var motionTouchEventY = 0f
     private var currentX = 0f
     private var currentY = 0f
     private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+    
+
 
     // Set up the paint with which to draw.
     private val paint = Paint().apply {
@@ -78,15 +81,33 @@ class MyCanvasView(context: Context) : View(context) {
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
     }
 
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        motionTouchEventX = event.x
-        motionTouchEventY = event.y
+//    override fun onTouchEvent(event: MotionEvent): Boolean {
+//        motionTouchEventX = event.x
+//        motionTouchEventY = event.y
+//
+//        when (event.action) {
+//            MotionEvent.ACTION_DOWN -> touchStart()
+//            MotionEvent.ACTION_MOVE -> touchMove()
+//            MotionEvent.ACTION_UP -> touchUp()
+//        }
+//        return true
+//    }
 
+    fun performClick(event: MotionEvent): Boolean {
+        val touchX = event.x
+        val touchY = event.y
         when (event.action) {
-            MotionEvent.ACTION_DOWN -> touchStart()
-            MotionEvent.ACTION_MOVE -> touchMove()
-            MotionEvent.ACTION_UP -> touchUp()
+            MotionEvent.ACTION_DOWN -> path.moveTo(touchX, touchY)
+            MotionEvent.ACTION_MOVE -> path.lineTo(touchX, touchY)
+            MotionEvent.ACTION_UP -> {
+                path.lineTo(touchX, touchY)
+                extraCanvas.drawPath(path, paint)
+                path.reset()
+            }
+            else -> return false
         }
+        //redraw
+        invalidate()
         return true
     }
 }
