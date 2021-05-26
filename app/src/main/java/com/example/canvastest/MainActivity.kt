806b1c7f1dcec1c7b594e217c88dcb4ef93e2b5f
Elsa Sheppard <sheppard.elsa03@gmail.com>
2021-05-26 13:37:07 -0700

    Drawing with clear button working

--- app/src/main/java/com/example/canvastest/MainActivity.kt
+++ app/src/main/java/com/example/canvastest/MainActivity.kt
@@ -2,44 +2,24 @@ package com.example.canvastest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
-import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    // toolbar vars
-    private lateinit var mtoolbarBottom: Toolbar
-    private lateinit var myCanvasView : MyCanvasView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        myCanvasView =  MyCanvasView(this)
-
-        // this makes drawing view fulscreen. not sure why its crossed out tho
-        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
-        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
         setContentView(R.layout.activity_main)
+//        val myCanvasView = MyCanvasView(this)
+//
+//        // this makes drawing view fulscreen. not sure why its crossed out tho
+//        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+//        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
 
-        // toolbar functions
-        mtoolbarBottom = findViewById(R.id.toolbar_bottom)
-        mtoolbarBottom.inflateMenu(R.menu.menu_drawing)
-        mtoolbarBottom.setOnMenuItemClickListener(Toolbar.OnMenuItemClickListener { item ->
-            handleDrawingIconTouched(item.itemId)
-            false
-        })
-    }
-
-    // toolbar function
-    private fun handleDrawingIconTouched(itemId: Int) {
-        when (itemId) {
-            R.id.action_brush -> {
-                myCanvasView?.setOnTouchListener { view, motionEvent ->
-                    myCanvasView?.performClick(motionEvent)!!
-
-                }
-            }
+        button_main_clear.setOnClickListener {
+            canvas_main_canvas.clearCanvas()
         }
     }
+
 }
--- app/src/main/java/com/example/canvastest/MyCanvasView.kt
+++ app/src/main/java/com/example/canvastest/MyCanvasView.kt
@@ -1,37 +1,51 @@
 package com.example.canvastest
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
 
-private const val STROKE_WIDTH = 12f // has to be float
+private const val STROKE_WIDTH = 12f
 
-class MyCanvasView(context: Context, attributeSet : AttributeSet) : View(context) {
+/**
+ * Custom view that follows touch events to draw on a canvas.
+ */
+class MyCanvasView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
+
+
+    // Holds the path you are currently drawing.
+    private var path = Path()
+
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+    private val whiteColor = ResourcesCompat.getColor(resources, R.color.white, null)
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
-    private val backgroundColor = ResourcesCompat.getColor(context.resources, R.color.colorBackground, null)
-    private val drawColor = ResourcesCompat.getColor(context.resources, R.color.colorPaint, null)
-    private var path = Path()
-    private var motionTouchEventX = 0f
-    private var motionTouchEventY = 0f
-    private var currentX = 0f
-    private var currentY = 0f
-    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
-    
+    private lateinit var frame: Rect
 
+//    var cc = false
+
+    fun clearCanvas() {
+//        cc = true
+//        invalidate()
+
+        frame = Rect(0, 0, width, height)
+        extraCanvas.drawRect(frame, white)
+    }
 
     // Set up the paint with which to draw.
+
+    private val white = Paint().apply {
+        color = whiteColor
+    }
+
     private val paint = Paint().apply {
         color = drawColor
         // Smooths out edges of what is drawn without affecting shape.
-        isAntiAlias = false // setting this to true smooths line edges
+        isAntiAlias = true
         // Dithering affects how colors with higher-precision than the device are down-sampled.
         isDither = true
         style = Paint.Style.STROKE // default: FILL
@@ -40,6 +54,67 @@ class MyCanvasView(context: Context, attributeSet : AttributeSet) : View(context
         strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
     }
 
+    /**
+     * Don't draw every single pixel.
+     * If the finger has has moved less than this distance, don't draw. scaledTouchSlop, returns
+     * the distance in pixels a touch can wander before we think the user is scrolling.
+     */
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    /**
+     * Called whenever the view changes size.
+     * Since the view starts out with no size, this is also called after
+     * the view has been inflated and has a valid size.
+     */
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+
+        // Calculate a rectangular frame around the picture.
+        //val inset = 40
+        //frame = Rect(inset, inset, width - inset, height - inset)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        // Draw the bitmap that has the saved path.
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+        // Draw a frame around the canvas.
+       // extraCanvas.drawRect(frame, paint)
+    }
+
+    /**
+     * No need to call and implement MyCanvasView#performClick, because MyCanvasView custom view
+     * does not handle click actions.
+     */
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+
+    /**
+     * The following methods factor out what happens for different touch events,
+     * as determined by the onTouchEvent() when statement.
+     * This keeps the when conditional block
+     * concise and makes it easier to change what happens for each event.
+     * No need to call invalidate because we are not drawing anything.
+     */
     private fun touchStart() {
         path.reset()
         path.moveTo(motionTouchEventX, motionTouchEventY)
@@ -56,9 +131,12 @@ class MyCanvasView(context: Context, attributeSet : AttributeSet) : View(context
             path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
             currentX = motionTouchEventX
             currentY = motionTouchEventY
-            // Draw the path in the extra bitmap to cache it.
+            // Draw the path in the extra bitmap to save it.
             extraCanvas.drawPath(path, paint)
         }
+        // Invalidate() is inside the touchMove() under ACTION_MOVE because there are many other
+        // types of motion events passed into this listener, and we don't want to invalidate the
+        // view for those.
         invalidate()
     }
 
@@ -67,47 +145,4 @@ class MyCanvasView(context: Context, attributeSet : AttributeSet) : View(context
         path.reset()
     }
 
-    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
-        super.onSizeChanged(width, height, oldWidth, oldHeight)
-        if (::extraBitmap.isInitialized) extraBitmap.recycle()
-
-        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        extraCanvas = Canvas(extraBitmap)
-        extraCanvas.drawColor(backgroundColor)
-    }
-
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
-    }
-
-//    override fun onTouchEvent(event: MotionEvent): Boolean {
-//        motionTouchEventX = event.x
-//        motionTouchEventY = event.y
-//
-//        when (event.action) {
-//            MotionEvent.ACTION_DOWN -> touchStart()
-//            MotionEvent.ACTION_MOVE -> touchMove()
-//            MotionEvent.ACTION_UP -> touchUp()
-//        }
-//        return true
-//    }
-
-    fun performClick(event: MotionEvent): Boolean {
-        val touchX = event.x
-        val touchY = event.y
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> path.moveTo(touchX, touchY)
-            MotionEvent.ACTION_MOVE -> path.lineTo(touchX, touchY)
-            MotionEvent.ACTION_UP -> {
-                path.lineTo(touchX, touchY)
-                extraCanvas.drawPath(path, paint)
-                path.reset()
-            }
-            else -> return false
-        }
-        //redraw
-        invalidate()
-        return true
-    }
 }
